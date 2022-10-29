4cb7daceb695bd9189d497c89f472486e345311d
joe-jr07 <110928270+joe-jr07@users.noreply.github.com>
2022-10-29 18:25:13 +0530

    Add files via upload

---
+++ Filter1/src/main/java/com/MyFilter.java
@@ -0,0 +1,51 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+@WebFilter("/home.html")
+public class MyFilter implements Filter {
+
+   
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpSession session = ((HttpServletRequest)request).getSession();
+		
+		String user = request.getParameter("name");
+		String password = request.getParameter("password");
+		
+		if(user != null && password != null && password.equals("admin")) {
+			session.setAttribute("user", user);
+			System.out.println("session name:"+ session.getAttribute("user"));
+			chain.doFilter(request, response);
+		}
+		else {
+			RequestDispatcher rd = request.getRequestDispatcher("index.html");
+			rd.include(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
