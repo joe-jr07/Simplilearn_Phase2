d4dd26dff918b5434e7fc9eee5ab5dd2219b6461
joe-jr07 <110928270+joe-jr07@users.noreply.github.com>
2022-11-03 19:08:47 +0530

    Delete PostBook directory

--- PostBook/src/main/java/model/PostDAO.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface PostDAO {
-	int addPost(Post post);
-	int updatePost(Post post);
-	int deletePost(Post post);
-}
--- PostBook/src/main/java/model/UserDAO.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface UserDAO {
-	boolean signIn(User user);
-	boolean signUp(User user);
-}
